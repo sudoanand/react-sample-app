f451a524b2bd0a6d287f66b1555223bb4495ed36
Anand Singh <hello@anandsingh.dev>
2024-11-02 15:25:39 +0530

    rock again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello PieHost! its rocking!
+          Hello PieHost! its rocking! and rocking again!
         </p>
         <a
           className="App-link"
