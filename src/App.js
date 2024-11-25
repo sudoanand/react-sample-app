73297e0e47452adf0ea7e7beff242b0542756b28
Anand Singh <hello@anandsingh.dev>
2024-11-25 19:30:17 +0530

    Hello from here

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, from pie host.
+          It works!
         </p>
         <a
           className="App-link"
